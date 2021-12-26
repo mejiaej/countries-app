bc875ea1d48502a4e5f645a8960db13a458d328b
Enrique <mejiaej@gmail.com>
2021-12-26 13:48:02 -0500

    feat: Add country card component

---
+++ src/components/common/CountryCard.tsx
@@ -0,0 +1,23 @@
+import './CountryCard.css';
+
+interface CountryCardProps {
+  officialName: string;
+  flagUrl: string;
+  className?: string;
+}
+
+const CountryCard = ({
+  officialName,
+  flagUrl,
+  className,
+}: CountryCardProps) => {
+
+  return (
+    <div className={`country-card-container ${className}`}>
+      <img className="flag" src={flagUrl} loading="lazy" alt={`${officialName} flag`}/>
+      <h4 className="mt-10">{officialName}</h4>
+    </div>
+  );
+};
+
+export { CountryCard };
---
+++ src/screens/CountriesScreen.tsx
@@ -0,0 +1,9 @@
+import { CountryCard } from '../components/common/CountryCard';
+
+const CountriesScreen = (): JSX.Element => {
+  return (
+    <CountryCard officialName="PERU" flagUrl="https://flagcdn.com/mt.svg" />
+  );
+};
+
+export { CountriesScreen };
